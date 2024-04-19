7b95ec0dcd81f49bc4021c033f8400e8eb4cfe04
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-04-19 12:55:00 +0200

    installed and configured oAuth2 dependency

--- simply-delicious-web-app/src/main.js
+++ simply-delicious-web-app/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import vue3GoogleLogin from 'vue3-google-login';
 
-createApp(App).mount('#app')
+const app=createApp(App);
+
+app.use(vue3GoogleLogin, {
+    clientId:'1059135692246-donflb298kffrdko5el2j7fsk4q31umc.apps.googleusercontent.com'
+});
+
+app.mount('#app');
