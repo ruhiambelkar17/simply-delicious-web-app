dcb2470323ff4f4d5e259cdabd765543ff7c946c
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-04-23 10:21:16 +0200

    updated sign out functionality

--- simply-delicious-web-app/src/components/NavBar.vue
+++ simply-delicious-web-app/src/components/NavBar.vue
@@ -29,7 +29,8 @@
         </div>
         <div class="d-inline-flex">
           <span class="pe-3">{{ user?.name }}</span>
-          <svg
+          <span v-if="user===null">
+            <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
             height="25"
@@ -43,6 +44,11 @@
               d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
             />
           </svg>
+          </span>
+          <span v-else >
+            <img class="img-fluid rounded-circle" width="30px" height="30px" :src="user?.picture" />
+          </span>
+          
           <div   class="dropdown">
             <span
               class="ps-2 dropdown-toggle"
