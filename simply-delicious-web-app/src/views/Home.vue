603d77ec7934e2cc78c8b7c2d684f50cadd68d85
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-05-01 19:08:01 +0200

    updated home ui

--- simply-delicious-web-app/src/views/Home.vue
+++ simply-delicious-web-app/src/views/Home.vue
@@ -19,10 +19,25 @@ export default {
   <div>
     <NavBar />
    <Carousel></Carousel>
-    <div class="my-3 d-flex justify-content-center">
-        <h3 class="text-center ">Recipes</h3>
+    <div class="container my-3 ">
+        <div class="d-flex justify-content-center"> 
+            <h3 class="text-center ">Recipes</h3>
+        </div>
         <br />
-        <hr style=" width: 200px;" class="" /> 
+        <div> 
+            <img src="../assets/images/Smoothie.jpeg" class="img-fluid recipe-img rounded shadow"  >
+        </div>
+        <div class="row"> 
+            <div class="col-md-12"> </div>
+        </div>
+       
     </div>
   </div>
 </template>
+
+<style scoped>
+.recipe-img {
+    width: 100%;
+    height: 300px;
+}
+</style>
