709702dc69c21f9751575a616ca99a91c9e33e7b
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-05-16 19:01:58 +0200

    updated ui

--- simply-delicious-web-app/src/views/Recipes.vue
+++ simply-delicious-web-app/src/views/Recipes.vue
@@ -1,9 +1,25 @@
 <script>
+import NavBar from "../components/NavBar.vue";
 export default {
-    
+    components:{
+        NavBar
+    }
 }
 </script>
 
 <template>
-    
+    <div class="">
+        <NavBar />
+    <div class="container">
+        <div class=" row m-5">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4 border rounded shadow">1</div>
+                <div class="col-md-4">2</div>
+                <div class="col-md-4">3</div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
 </template>
