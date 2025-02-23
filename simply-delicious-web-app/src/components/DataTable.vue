e8e68e61ff65fc15ecdccfc3f0c38a269aee9984
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2025-02-24 00:16:11 +0530

    added table

---
+++ simply-delicious-web-app/src/components/DataTable.vue
@@ -0,0 +1,90 @@
+<script>
+export default {
+  created() {
+    this.getHeadings();
+    this.getTableData();
+  },
+  data() {
+    return {
+      tableData: [
+        { srNo: 1, recipeType: "veg", recipeName: "Paneer Tikka", ratings: 5 },
+        { srNo: 2, recipeType: "non-veg", recipeName: "Chicken Tikka", ratings: 4.5 },
+        { srNo: 3, recipeType: "veg", recipeName: "Veg Kolhapuri", ratings: 4 },
+        { srNo: 4, recipeType: "non-veg", recipeName: "Chicken Bhuna", ratings: 5 },
+      ],
+      headings: [],
+      tableBodyData:[]
+    };
+  },
+  methods: {
+    getHeadings() {
+      for (let i = 0; i <= this.tableData.length - 1; i++) {
+        //this.headings.push(Object.keys(this.tableData[i]));
+        this.headings = Object.keys(this.tableData[i]);
+      }
+      console.log("heading arr", this.headings);
+    },
+    getTableData(){
+        for (let data in this.tableData){
+            
+            let x=this.tableData[data];
+            console.log("data",x)
+            let dd;
+            for (let d in x){
+                console.log("d",x[d])
+                dd=x[d]
+                
+            }
+            this.tableBodyData+=this.tableBodyData.push(dd);
+            // for (let heading in this.headings){
+            //     let y=this.headings[heading];
+                
+            //     console.log("x",y.x)
+            // }
+            // let x=this.headings[i];
+            // console.log("x",this.tableData[i].x)
+            // this.tableBodyData.push(this.tableData[i]?.x)
+        }
+        console.log("gettable data",this.tableBodyData)
+    }
+  },
+};
+</script>
+
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <template v-for="(item, index) in headings" :key="index">
+          <th scope="col">{{ item }}</th>
+        </template>
+      </tr>
+    </thead>
+
+    <tbody>
+      <!-- <template v-for="(item1,index1) in headings" :key="index1"> -->
+        <template v-for="(item,index) in tableData" :key="index" >
+            
+            <tr>
+        <td>{{ item?.srNo }}</td>
+        <td>{{ item?.srNo}}</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+        </template>
+        
+      <!-- </template> -->
+      <!-- <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr> -->
+    </tbody>
+  </table>
+</template>
--- simply-delicious-web-app/src/views/RecipesCatalog.vue
+++ simply-delicious-web-app/src/views/RecipesCatalog.vue
@@ -1,44 +1,24 @@
-<template>
-    <div>
-        <h1 class="text-center">Recipes</h1>
-        <hr />
-        <div>
-            <b-table
-        :items="items"
-        :fields="fields"
-        :sort-by="sortBy"
-        :sort-desc="sortDesc"
-        responsive="sm"
-      ></b-table>
-        </div>
-  
-      <div>
-        Sorting By: <b>{{ sortBy }}</b>, Sort Direction:
-        <b>{{ sortDesc ? 'Descending' : 'Ascending' }}</b>
-      </div>
-    </div>
-    
-</template>
-
-  <script>
+<script>
+  import DataTable from '../components/DataTable.vue';
     export default {
+        components:{DataTable},
       data() {
         return {
-          sortBy: 'age',
-          sortDesc: false,
-          fields: [
-            { key: 'last_name', sortable: true },
-            { key: 'first_name', sortable: true },
-            { key: 'age', sortable: true },
-            { key: 'isActive', sortable: false }
-          ],
-          items: [
-            { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-            { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-            { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-            { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-          ]
+          
         }
       }
     }
   </script>
+
+<template>
+    <div>
+        <h1 class="text-center">Recipes</h1>
+        <hr />
+        <div class="col-md-10 offset-1">
+            <DataTable></DataTable>
+        </div>
+    </div>
+    
+</template>
+
+  
